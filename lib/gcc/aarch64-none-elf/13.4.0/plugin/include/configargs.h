1a9bd5932a15c0afa865e7f9c75202c300625341
Shanmin Zhang <zhangshanmin@xiaomi.com>
2025-07-17 01:49:20 +0800

    GCC: update to version to 13.4.0 (aarch64-none-elf) 20250718
    Signed-off-by: Shanmin Zhang <zhangshanmin@xiaomi.com>

---
+++ lib/gcc/aarch64-none-elf/13.4.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/Users/shanmin/gcc-build/gcc/configure --prefix=/Users/shanmin/gcc-build/out/darwin-aarch64/aarch64-none-elf --host=arm64-apple-darwin --build=arm64-apple-darwin --target=aarch64-none-elf --with-zstd=/Users/shanmin/gcc-build/out/darwin-aarch64/aarch64-none-elf --with-gmp=/Users/shanmin/gcc-build/out/darwin-aarch64/aarch64-none-elf --with-mpfr=/Users/shanmin/gcc-build/out/darwin-aarch64/aarch64-none-elf --with-mpc=/Users/shanmin/gcc-build/out/darwin-aarch64/aarch64-none-elf --with-gnu-as --with-gnu-ld --disable-werror --disable-shared --disable-nls --disable-threads --disable-tls --with-system-zlib --enable-gnu-unique-object --enable-linker-build-id --with-newlib --with-sysroot=/Users/shanmin/gcc-build/out/darwin-aarch64/aarch64-none-elf/aarch64-none-elf --with-native-system-header-dir=/include --with-headers=yes --enable-languages=c,c++,lto --enable-checking=release";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
